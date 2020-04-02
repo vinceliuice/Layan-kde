ab5b33028acf3126b45fb62ac6942a5cbfdea9fa
vinceliuice <vinceliuice@hotmail.com>
2020-04-02 11:51:09 +0800

    Add solid version adn LAF issues

--- install.sh
+++ install.sh
@@ -25,28 +25,19 @@ THEME_NAME=Layan
 install() {
   local name=${1}
 
-  local AURORAE_THEME="${AURORAE_DIR}/${name}"
-  local PLASMA_THEME="${PLASMA_DIR}/${name}"
-  local LOOKFEEL_THEME="${LOOKFEEL_DIR}/com.github.vinceliuice.${name}"
-
-  [[ -d ${AURORAE_THEME} ]] && rm -rf ${AURORAE_THEME}
-  [[ -d ${PLASMA_THEME} ]] && rm -rf ${PLASMA_THEME}
-  [[ -d ${LOOKFEEL_THEME} ]] && rm -rf ${LOOKFEEL_THEME}
-  [[ -d ${KVANTUM_THEME}/${name} ]] && rm -rf ${KVANTUM_THEME}/${name}
-  [[ -d ${SCHEMES_DIR}/${name}.colors ]] && rm -rf ${SCHEMES_DIR}/${name}.colors
-
   mkdir -p                                                                           ${AURORAE_DIR}
   mkdir -p                                                                           ${SCHEMES_DIR}
   mkdir -p                                                                           ${PLASMA_DIR}
   mkdir -p                                                                           ${LOOKFEEL_DIR}
   mkdir -p                                                                           ${KVANTUM_DIR}
 
-  cp -ur ${SRC_DIR}/aurorae/themes/*                                                 ${AURORAE_DIR}
-  cp -ur ${SRC_DIR}/color-schemes/*.colors                                           ${SCHEMES_DIR}
-  cp -ur ${SRC_DIR}/Kvantum/*                                                        ${KVANTUM_DIR}
-  cp -ur ${SRC_DIR}/plasma/desktoptheme/${name}${ELSE_DARK}                          ${PLASMA_DIR}
-  cp -ur ${SRC_DIR}/color-schemes/Layan.colors                                       ${PLASMA_DIR}/${name}/colors
-  cp -ur ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}              ${LOOKFEEL_DIR}
+  cp -r ${SRC_DIR}/aurorae/themes/*                                                  ${AURORAE_DIR}
+  cp -r ${SRC_DIR}/color-schemes/*.colors                                            ${SCHEMES_DIR}
+  cp -r ${SRC_DIR}/Kvantum/*                                                         ${KVANTUM_DIR}
+  cp -r ${SRC_DIR}/plasma/desktoptheme/*                                             ${PLASMA_DIR}
+  cp -r ${SRC_DIR}/color-schemes/${name}.colors                                      ${PLASMA_DIR}/${name}/colors
+  cp -r ${SRC_DIR}/color-schemes/${name}.colors                                      ${PLASMA_DIR}/${name}-solid/colors
+  cp -r ${SRC_DIR}/plasma/look-and-feel/*                                            ${LOOKFEEL_DIR}
 }
 
 echo "Installing '${THEME_NAME} kde themes'..."
---
+++ plasma/look-and-feel/com.github.vinceliuice.Layan/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,152 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "540",
+                    "DialogWidth": "720"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "42"
+                },
+                "/General": {
+                    "ToolBoxButtonState": "topcenter",
+                    "ToolBoxButtonX": "462",
+                    "ToolBoxButtonY": "32"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///usr/share/wallpapers/Elarun/contents/images/2560x1600.png"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "left",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "100"
+                        },
+                        "/Configuration/General": {
+                            "favoritesPortedToKAstats": "true",
+                            "systemApplications": "systemsettings.desktop,org.kde.kinfocenter.desktop"
+                        },
+                        "/Configuration/Shortcuts": {
+                            "global": "Alt+F1"
+                        },
+                        "/Shortcuts": {
+                            "global": "Alt+F1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "42"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.appmenu"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "42"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "52"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "72"
+                        },
+                        "/Configuration/Appearance": {
+                            "displayTimezoneAsCode": "false",
+                            "spinboxHorizontalPercentage": "60",
+                            "use24hFormat": "2"
+                        },
+                        "/Configuration/ConfigDialog": {
+                            "DialogHeight": "540",
+                            "DialogWidth": "720"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.splitdigitalclock"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "52"
+                        }
+                    },
+                    "plugin": "org.kde.milou"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "74",
+                    "DialogWidth": "1366"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "42"
+                }
+            },
+            "height": 1.7777777777777777,
+            "hiding": "normal",
+            "location": "top",
+            "maximumLength": 75.88888888888889,
+            "minimumLength": 75.88888888888889,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
