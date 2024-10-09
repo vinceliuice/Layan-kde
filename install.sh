6c5f5d3c8908c1e6bf7ba743cd63965faa1b66ac
vinceliuice <vinceliuice@hotmail.com>
2024-10-09 11:18:18 +0800

    Update #15

--- install.sh
+++ install.sh
@@ -39,21 +39,21 @@ install() {
 
   [[ ${color} == '-light' ]] && local ELSE_COLOR='Light'
 
-  [[ -d ${AURORAE_DIR}/${name}${color} ]] && rm -rf ${AURORAE_DIR}/${name}${color}
+  [[ -d ${AURORAE_DIR}/${name}${color} ]] && rm -rf ${AURORAE_DIR}/${name}*
   [[ -d ${PLASMA_DIR}/${name}${color} ]] && rm -rf ${PLASMA_DIR}/${name}${color}
   [[ -f ${SCHEMES_DIR}/${name}${ELSE_COLOR}.colors ]] && rm -rf ${SCHEMES_DIR}/${name}${ELSE_COLOR}.colors
   [[ -d ${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${color} ]] && rm -rf ${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${color}
   [[ -d ${KVANTUM_DIR}/${name} ]] && rm -rf ${KVANTUM_DIR}/${name}*
   [[ -d ${WALLPAPER_DIR}/${name}${color} ]] && rm -rf ${WALLPAPER_DIR}/${name}${color}
 
-  cp -r ${SRC_DIR}/aurorae/themes/${name}${color}                                    ${AURORAE_DIR}
-  cp -r ${SRC_DIR}/color-schemes/${name}${ELSE_COLOR}.colors                         ${SCHEMES_DIR}
-  cp -r ${SRC_DIR}/Kvantum/*                                                         ${KVANTUM_DIR}
-  cp -r ${SRC_DIR}/plasma/desktoptheme/common                                        ${PLASMA_DIR}/${name}${color}
-  cp -r ${SRC_DIR}/plasma/desktoptheme/${name}${color}/*                             ${PLASMA_DIR}/${name}${color}
-  cp -r ${SRC_DIR}/color-schemes/${name}${ELSE_COLOR}.colors                         ${PLASMA_DIR}/${name}${color}/colors
-  cp -r ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${color}       ${LOOKFEEL_DIR}
-  cp -r ${SRC_DIR}/wallpaper/${name}${color}                                         ${WALLPAPER_DIR}
+  cp -rf ${SRC_DIR}/aurorae/themes/${name}*                                           ${AURORAE_DIR}
+  cp -rf ${SRC_DIR}/color-schemes/${name}${ELSE_COLOR}.colors                         ${SCHEMES_DIR}
+  cp -rf ${SRC_DIR}/Kvantum/${name}*                                                  ${KVANTUM_DIR}
+  cp -rf ${SRC_DIR}/plasma/desktoptheme/common                                        ${PLASMA_DIR}/${name}${color}
+  cp -rf ${SRC_DIR}/plasma/desktoptheme/${name}${color}/*                             ${PLASMA_DIR}/${name}${color}
+  cp -rf ${SRC_DIR}/color-schemes/${name}${ELSE_COLOR}.colors                         ${PLASMA_DIR}/${name}${color}/colors
+  cp -rf ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${color}       ${LOOKFEEL_DIR}
+  cp -rf ${SRC_DIR}/wallpaper/${name}${color}                                         ${WALLPAPER_DIR}
 }
 
 echo "Installing '${THEME_NAME} kde themes'..."
--- uninstall.sh
+++ uninstall.sh
@@ -24,24 +24,26 @@ fi
 SRC_DIR=$(cd $(dirname $0) && pwd)
 
 THEME_NAME=Layan
+COLOR_VARIANTS=('' '-light')
 
 uninstall() {
   local name=${1}
+  local color=${2}
 
-  local AURORAE_THEME="${AURORAE_DIR}/${name}"
-  local PLASMA_THEME="${PLASMA_DIR}/${name}"
-  local LOOKFEEL_THEME="${LOOKFEEL_DIR}/com.github.vinceliuice.${name}"
+  [[ ${color} == '-light' ]] && local ELSE_COLOR='Light'
 
-  [[ -d ${AURORAE_THEME} ]] && rm -rfv ${AURORAE_THEME}
-  [[ -d ${PLASMA_THEME} ]] && rm -rfv ${PLASMA_THEME}
-  [[ -d ${LOOKFEEL_THEME} ]] && rm -rfv ${LOOKFEEL_THEME}
-  [[ -d ${KVANTUM_THEME}/${name} ]] && rm -rfv ${KVANTUM_THEME}/${name}
-  [[ -d ${SCHEMES_DIR}/${name}.colors ]] && rm -rfv ${SCHEMES_DIR}/${name}.colors
-  [[ -d ${WALLPAPER_DIR}/Layan ]] && rm -rfv ${WALLPAPER_DIR}/{Layan,Layan-light}
+  [[ -d ${AURORAE_DIR}/${name}${color} ]] && rm -rfv ${AURORAE_DIR}/${name}*
+  [[ -d ${PLASMA_DIR}/${name}${color} ]] && rm -rfv ${PLASMA_DIR}/${name}${color}
+  [[ -f ${SCHEMES_DIR}/${name}${ELSE_COLOR}.colors ]] && rm -rfv ${SCHEMES_DIR}/${name}${ELSE_COLOR}.colors
+  [[ -d ${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${color} ]] && rm -rfv ${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${color}
+  [[ -d ${KVANTUM_DIR}/${name} ]] && rm -rfv ${KVANTUM_DIR}/${name}*
+  [[ -d ${WALLPAPER_DIR}/${name}${color} ]] && rm -rfv ${WALLPAPER_DIR}/${name}${color}
 }
 
 echo "Uninstalling '${THEME_NAME} kde themes'..."
 
-uninstall "${name:-${THEME_NAME}}"
+for color in "${colors[@]:-${COLOR_VARIANTS[@]}}"; do
+  uninstall "${name:-${THEME_NAME}}" "${color}"
+done
 
 echo "Uninstall finished..."
